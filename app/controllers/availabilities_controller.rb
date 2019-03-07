37ed4d06fac874024801302a42774617220f135a
Lucas Martins <lucaslealm@gmail.com>
2019-03-07 11:12:56 -0300

    routes created and authentication update

--- app/controllers/availabilities_controller.rb
+++ app/controllers/availabilities_controller.rb
@@ -1,4 +1,5 @@
 class AvailabilitiesController < ApplicationController
+  skip_before_action :authenticate_user!, only: [:index, :show]
   def index
     @availabilities = Availability.all
   end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  resources :availabilities, except: [:edit, :update, :destroy] do
+    resources :bookings, only: [:index, :new, :create]
+  end
 end
