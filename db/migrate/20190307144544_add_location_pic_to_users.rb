cbf66f01e6f4f4d5a6432ba555ba017880e9d2ab
Lucas Martins <lucaslealm@gmail.com>
2019-03-07 11:49:28 -0300

    added location_pic and user_pic to users table

---
+++ db/migrate/20190307144544_add_location_pic_to_users.rb
@@ -0,0 +1,5 @@
+class AddLocationPicToUsers < ActiveRecord::Migration[5.2]
+  def change
+    add_column :users, :location_pic, :string
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_06_203834) do
+ActiveRecord::Schema.define(version: 2019_03_07_144747) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -48,6 +48,8 @@ ActiveRecord::Schema.define(version: 2019_03_06_203834) do
     t.string "phone_number"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "location_pic"
+    t.string "user_pic"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
