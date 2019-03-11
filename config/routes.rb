bb5deb48050f81418e6ec958b6c6283f98e1b380
Lucas Martins <lucaslealm@gmail.com>
2019-03-11 17:45:31 -0300

    routes created

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,5 @@ Rails.application.routes.draw do
   resources :availabilities, except: [:edit, :update, :destroy] do
     resources :bookings, only: [:new, :create]
   end
-
+  get 'profile', to: 'pages#profile', as: :profile
 end
