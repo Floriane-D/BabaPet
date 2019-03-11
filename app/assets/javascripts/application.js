73632d8d93a30b5e2f92d04091a15856d3bfa9ae
Camille Huret <churet@me.com>
2019-03-11 15:57:50 -0300

    date range picker ok, geminstalled, picker in the availability show page, formatting not great, need improvement

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,2 +1,6 @@
 //= require rails-ujs
 //= require_tree .
+//= require jquery
+//= require moment
+//= require daterangepicker
+$('input[name="availability_start_date"]').daterangepicker();
--- app/models/availability.rb
+++ app/models/availability.rb
@@ -1,4 +1,10 @@
 class Availability < ApplicationRecord
   belongs_to :user
   has_many :bookings
+
+  def start_time
+    start_date ##Where 'start' is a attribute of type 'Date' accessible through MyModel's relationship
+  end
 end
+
+
