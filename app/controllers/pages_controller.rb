4e0f679c922e0c2f6f028683637f827b2b8d9a7a
Floriane Didier <florianedidier@gmail.com>
2019-03-12 13:02:54 -0300

    search try

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,4 +2,22 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: :home
   def home
   end
+
+  def search
+    search_start_date = params[:search][:start_date]
+    search_end_date = params[:search][:end_date]
+    availabilities_without_location_criteria = Availability.available_between(search_start_date, search_end_date)
+
+    search_address = params[:search][:address]
+    search_distance = params[:search][:distance]
+    search_coordinates = Geocoder.search(search_address).first.coordinates
+
+    availabilities_with_location_criteria = []
+
+    availabilities_without_location_criteria.each do |availability|
+      if availability.user.distance_to(search_coordinates) < search_distance
+        availabilities_with_location_criteria << availability
+      end
+    end
+  end
 end
--- app/models/availability.rb
+++ app/models/availability.rb
@@ -2,6 +2,10 @@ class Availability < ApplicationRecord
   belongs_to :user
   has_many :bookings
 
+  def self.available_between(start_date, end_date)
+    where("start_date < ? AND end_date > ?", start_date, end_date)
+  end
+
   def start_time
     start_date ##Where 'start' is a attribute of type 'Date' accessible through MyModel's relationship
   end
